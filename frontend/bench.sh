fb949ae1e877c9078666b6dfc8cbc227437a2985
lixizan <lixizan@webull.com>
2023-08-23 14:56:09 +0800

    添加延迟图

--- frontend/bench.sh
+++ frontend/bench.sh
@@ -48,4 +48,18 @@ do
     sleep ${SLEEP}
 done
 
+for loop in 1 2 3 4 5
+do
+    wsbench echo -c ${C} -n 100 -p 8000 -u ws://localhost:8000/connect -o ./logs/gws.log
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n 100 -p 8000 -u ws://localhost:8001/connect -o ./logs/gorilla.log
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n 100 -p 8000 -u ws://localhost:8002/connect -o ./logs/nhooyr.log
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n 100 -p 8000 -u ws://localhost:8003/connect -o ./logs/gobwas.log
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n 100 -p 8000 -u ws://localhost:8004/connect -o ./logs/nbio.log
+    sleep ${SLEEP}
+done
+
 node statistics.js
