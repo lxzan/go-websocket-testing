ea7fdc176a0e9228c93330b12c6232ec6e53ea40
lixizan <lixizan@webull.com>
2023-11-09 09:22:13 +0800

    upgrade

--- frontend/bench.sh
+++ frontend/bench.sh
@@ -3,49 +3,49 @@
 C=1000
 N=1000000
 SLEEP=3
-Concurrency=4
+Concurrency=8
 
 rm ./logs/*
 
 for loop in 1 2 3 4 5
 do
-    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency}
-    sleep ${SLEEP}
-done
-
-for loop in 1 2 3 4 5
-do
-    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency}
-    sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency}
-    sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency}
-    sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency}
-    sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 500 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
 done
 
 for loop in 1 2 3 4 5
 do
-    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
-    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency}
+    wsbench echo -c ${C} -n ${N} -p 2000 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency} --latency
+    sleep ${SLEEP}
+done
+
+for loop in 1 2 3 4 5
+do
+    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8000/connect -o ./logs/gws.log --concurrency=${Concurrency} --latency
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8001/connect -o ./logs/gorilla.log --concurrency=${Concurrency} --latency
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8002/connect -o ./logs/nhooyr.log --concurrency=${Concurrency} --latency
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8003/connect -o ./logs/gobwas.log --concurrency=${Concurrency} --latency
+    sleep ${SLEEP}
+    wsbench echo -c ${C} -n ${N} -p 8000 -u ws://127.0.0.1:8004/connect -o ./logs/nbio.log --concurrency=${Concurrency} --latency
     sleep ${SLEEP}
 done
 
--- go.mod
+++ go.mod
@@ -3,13 +3,12 @@ module github.com/lxzan/go-websocket-testing
 go 1.16
 
 require (
-	github.com/cloudwego/hertz v0.6.4
-	github.com/gobwas/ws v1.3.0
-	github.com/gorilla/websocket v1.5.0
-	github.com/hertz-contrib/websocket v0.0.1
-	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/lesismal/nbio v1.3.17
-	github.com/lxzan/gws v1.6.10
+	github.com/gobwas/ws v1.3.1
+	github.com/gorilla/websocket v1.5.1
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/lesismal/nbio v1.3.19
+	github.com/lxzan/gws v1.6.13
 	github.com/prometheus/client_golang v1.14.0
-	nhooyr.io/websocket v1.8.7
+	golang.org/x/net v0.18.0 // indirect
+	nhooyr.io/websocket v1.8.10
 )
